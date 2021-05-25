e6fc08e2a0807d1da54ed70f0fd2ebf10e93c21d
myName <myEmail>
2021-05-25 12:11:49 +0700

    lesson1 refactoring

--- lesson1.js
+++ lesson1.js
@@ -2,72 +2,61 @@
 const colors = require("colors/safe");
 
 //функция поиска простых чисел
-function FindSimpleNumbers(startNumber, endNumber) {
-    let SimpleNumbers = []; //массив простых чисел
-    let CountSimpleNumbers = 0; //количество простых чисел
-    let IsSimple = false; // переменная для проверки числа на прототу
-
-    //так как 1 не является простым числом, проверки будут происходить начиная с 2
-    if(startNumber==1){
-        startNumber=2;
-    }
+function findSimpleNumbers(startNumber, endNumber) {
+    let simpleNumbers = []; //массив простых чисел
+    let countSimpleNumbers = 0; //количество простых чисел
+    let isSimple = false; // переменная для проверки числа на прототу
 
     for(let i=startNumber; i<=endNumber; i++){
+        //так как 1 не является простым числом, проверки будут происходить начиная с 2
+        if(i==1){
+            continue;
+        }
         //Если проверка ниже на простое чилсо не пройдет - то число будет простым.
-        IsSimple=true;
+        isSimple=true;
         //проверяем простое ли число. Если число делится хотя бы на одно число - то не простое.
         for(let j=2; j<i; j++){
             if(i%j==0){
-                IsSimple=false;
+                isSimple=false;
             }
         }
         //добавляем в массив чисел просто число
-        if(IsSimple==true){
-            SimpleNumbers[CountSimpleNumbers]=i;
-            CountSimpleNumbers++;
+        if(isSimple==true){
+            simpleNumbers[countSimpleNumbers]=i;
+            countSimpleNumbers++;
         }
     }
-    return SimpleNumbers;
-}
-
-//проверка, является ли значение целым числом
-function NumberIsInteger(a) {
-    if(Number.isInteger(a)){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return simpleNumbers;
 }
 
 //Распечатать цветной массив
-function PrintColorArray(NumbersArray) {
+function printColorArray(numbersArray) {
 
-    for(let i=0; i<NumbersArray.length; i++){
+    for(let i=0; i<numbersArray.length; i++){
         switch (i%3) {
-            case 0: console.log(colors.green(NumbersArray[i])); break;
-            case 1: console.log(colors.yellow(NumbersArray[i])); break;
-            case 2: console.log(colors.red(NumbersArray[i])); break;
+            case 0: console.log(colors.green(numbersArray[i])); break;
+            case 1: console.log(colors.yellow(numbersArray[i])); break;
+            case 2: console.log(colors.red(numbersArray[i])); break;
         }
     }
 }
 
 //запуск программы
 function run(startNumber,endNumber) {
-
-    if(NumberIsInteger(startNumber) && NumberIsInteger(endNumber) ){
-        let SimpleNumbers = FindSimpleNumbers(startNumber,endNumber);
-        if(SimpleNumbers.length==0){
-            console.log(colors.red("Простых чисел между " + startNumber + " и " +  endNumber + " нет."));
-        }else{
-            console.log("Простые числа между " + startNumber + " и " +  endNumber + ":");
-            PrintColorArray(SimpleNumbers);
-        }
-
+    if(!Number.isInteger(startNumber) || !Number.isInteger(endNumber) ){
+        console.log(colors.red("Ошибка: Одно из введенных Вами значений не является целым числом!"));
+        return;
     }
-    else{
-        console.log(colors.red("Ошибка: Одно из введенных Вами значения не является целым числом!"));
+
+    let simpleNumbers = findSimpleNumbers(startNumber,endNumber);
+
+    if(simpleNumbers.length==0){
+        console.log(colors.red(`Простых чисел между ${startNumber} и ${endNumber} нет.`));
+        return;
     }
+
+    console.log(`"Простые числа между ${startNumber} и  ${endNumber}:`);
+    printColorArray(simpleNumbers);
 }
 
 
