45c609d39a9a1628d4ad766632699a6967c5e699
myName <myEmail>
2021-06-03 14:34:37 +0700

    lesson3 variant2

--- lesson3.js
+++ lesson3.js
@@ -1,35 +1,21 @@
 const fs = require('fs');
+const split2 = require("split2")
 
-const readStream = new fs.ReadStream('./access.log', 'utf8');
+const firstLog = fs.createWriteStream('89.123.1.41_requests.log', { flags: 'a', encoding: 'utf8' });
+const secondLog = fs.createWriteStream('34.48.240.111_requests.log', { flags: 'a', encoding: 'utf8' });
 
-const {Transform} = require('stream');
-
-
-const transformStream = new Transform({
-    transform(chunk, encoding, callback) {
-
-        const firstLog = chunk.toString().match(/89.123.1.41.*[\n\r]/g);
-        if(firstLog){
-            fs.writeFile('./89.123.1.41_requests.log', firstLog, {flag: 'a'}, (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            });
+fs.createReadStream('./access.log', 'utf8')
+    .pipe(split2())
+    .on('data', function (chunk) {
+        let firstLogText = chunk.toString().match(/89.123.1.41.*/g);
+        if(firstLogText){
+            firstLog.write(chunk);
+            firstLog.write("\n");
         }
 
-
-        const secondLog = chunk.toString().match(/34.48.240.111.*[\n\r]/g);
-        if(secondLog){
-            fs.writeFile('./34.48.240.111_requests.log', secondLog, {flag: 'a'}, (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            });
+        let secondLogText = chunk.toString().match(/34.48.240.111.*/g);
+        if(secondLogText){
+            secondLog.write(chunk);
+            secondLog.write("\n");
         }
-
-
-        callback();
-    }
-});
-
-readStream.pipe(transformStream);
+    });
